0527670ec9782a91e75a0280c19203fec303b08a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-02 12:49:56 -0800

    added the product model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title: models.CharField(max_length=50,null=False)
+    price: models.ImageField()
+    description: models.TextField()
+    added_date: models.DateField(auto_now=True)
