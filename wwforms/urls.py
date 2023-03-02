4de079713efcd6927341f5f0bf57a9bbed0cd916
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-02 13:04:41 -0800

    added custome 500 error page

--- wwforms/urls.py
+++ wwforms/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('home/',include('home.urls'))
 ]
 handler404="wwforms.views.handler404"
+handler500="wwforms.views.handler500"
--- wwforms/views.py
+++ wwforms/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def handler404(request,exception):
     return HttpResponse("404: This page does not exist")
+
+def handler500(request,exception):
+    return HttpResponse("500: The server encountered an error")
